08cdf5241ca0a6c2f5cf7c883b30a1e5f861d00d
Bruce Christie <bruce.christie@mobilelive.ca>
2018-02-06 21:35:34 -0500

    Adding login form...

---
+++ src/app/top-menu/top-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-menu',
+  template: `
+    <h4>
+      top-menu Works!
+    </h4>
+  `,
+  styles: []
+})
+export class TopMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
